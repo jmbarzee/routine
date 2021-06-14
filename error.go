fddeb0dc35ec7e2696adf5392b93d47c5df7617b
Jacob <jmbarzee@gmail.com>
2021-06-14 11:14:23 -0600

    Expose MultiError.Errs

--- error.go
+++ error.go
@@ -11,19 +11,19 @@ var _ error = MultiError{}
 // produce multiple errors during their operation but
 // are confined to produce a single error.
 type MultiError struct {
-	errs []error
+	Errs []error
 }
 
 func (err MultiError) Error() string {
 	s := ""
-	for i, e := range err.errs {
+	for i, e := range err.Errs {
 		s += fmt.Sprintf("error[%v]: %s", i, e.Error())
 	}
 	return s
 }
 
 func (err MultiError) As(target interface{}) bool {
-	for _, e := range err.errs {
+	for _, e := range err.Errs {
 		if errors.As(e, target) {
 			return true
 		}
@@ -32,7 +32,7 @@ func (err MultiError) As(target interface{}) bool {
 }
 
 func (err MultiError) Is(target error) bool {
-	for _, e := range err.errs {
+	for _, e := range err.Errs {
 		if errors.Is(e, target) {
 			return true
 		}
